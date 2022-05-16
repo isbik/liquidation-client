5e079a1a9c0ec39264187fef23dcd8bbcbee42fe
dmitry <bikishovd>
2022-05-17 04:38:22 +0500

    add env

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { toast } from "react-toastify";
 
-const BASE_URL = "http://localhost:4200";
+const BASE_URL = process.env.NEXT_API_URL;
 
 const api = axios.create({
   baseURL: BASE_URL,
